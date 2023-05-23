261ecf585a54eac14ee0148b34cbdd4e2918b36c
lqmeyers <lqmeyers@gmail.com>
2023-05-23 11:30:52 -0700

    new data structure names to match_copy

--- copyMatch.sh
+++ copyMatch.sh
@@ -1,14 +1,14 @@
-#!/bin/bash/
+\#!/bin/bash/
 
 sum=0
-for f in ~/CNN_dev/paintDetect/images/testing/*; do
-	if [[ -f "./${f:50:-4}.png" ]]; then
-		echo "File ./${f:50:-4}.png exists. Performing action..."
+for f in ~/paintDetect/data/images/training/*; do
+	if [[ -f "./${f:48:-4}.pl.Thorax.png" ]]; then
+		echo "File ./${f:48:-4}.pl.png exists. Performing action..."
 	# Perform your action here
-	cp ./${f:50:-4}.Paint.png ~/CNN_dev/paintDetect/masks/testing/${f:50:-4}.Paint.png
+	cp ./${f:48:-4}.pl.Thorax.png /home/lqmeyers/paintDetect/data/thorax_only_masks/training/${f:48:-4}.Thorax.png
 	sum=$((sum+1))
 	else
-		echo "File ./${f:50:-4}.png not found. Skipping action."
+		echo "File ./${f:48:-4}.png not found. Skipping action."
 	fi
 done
 echo "Number of matches = "$sum
