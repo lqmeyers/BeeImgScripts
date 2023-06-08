a4101713dc4657c6a22888405dc0429d1c0a7104
lqmeyers <lqmeyers@gmail.com>
2023-06-08 11:37:31 -0700

    adding more doc to copyMatch

--- copyMatch.sh
+++ copyMatch.sh
@@ -1,14 +1,19 @@
-\#!/bin/bash/
+#!/bin/bash/
 
+#run this script in the local dir of images to be copied
+#this means doing it both for baby bees and flowerpatch
+
+#alter the target paths, filename modifications, and lens based on target dirs.
+#remember to adjust start int of path slicing for len dif of testing vs training (currently 47 vs 48)
 sum=0
-for f in ~/paintDetect/data/images/training/*; do
-	if [[ -f "./${f:48:-4}.pl.Thorax.png" ]]; then
-		echo "File ./${f:48:-4}.pl.png exists. Performing action..."
+for f in ~/paintDetect/data/images/testing/*; do
+	if [[ -f "./${f:47:-4}.png" ]]; then
+		echo "File ./${f:47:-4}.png exists. Performing action..."
 	# Perform your action here
-	cp ./${f:48:-4}.pl.Thorax.png /home/lqmeyers/paintDetect/data/thorax_only_masks/training/${f:48:-4}.Thorax.png
+	cp ./${f:47:-4}.png /home/lqmeyers/paintDetect/data/full_masks/testing/${f:47:-4}.png
 	sum=$((sum+1))
 	else
-		echo "File ./${f:48:-4}.png not found. Skipping action."
+		echo "File ./${f:47:-4}.png not found. Skipping action."
 	fi
 done
 echo "Number of matches = "$sum
